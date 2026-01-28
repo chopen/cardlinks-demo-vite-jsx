5712603f9198bd2946c537d812b9f88e30292aa1
chopen <chris.hopen@gmail.com>
2026-01-28 13:39:47 -0800

    Update sessionStorage.jsx

--- src/utils/sessionStorage.jsx
+++ src/utils/sessionStorage.jsx
@@ -28,17 +28,16 @@ export default function useGlobalSessionState() {
 // Create a provider component that will wrap the parts of your app needing access
 export const GlobalSessionStateProvider = ({ children }) => {
   const [cardLinksData, setCardLinksData] = useState(() => {
-    console.log('SessionStorageContext: ', SessionStorageContext);
     try {
       let theCardLinksData = SessionStorageContext._currentValue.cardLinksData;
       if (typeof sessionStorage != 'undefined') {
         let savedCardLinksData = sessionStorage.getItem('cardLinksData');
         if (savedCardLinksData) {
-          console.log('savedCardLinksData: ', savedCardLinksData);
+          //console.log('savedCardLinksData: ', savedCardLinksData);
           theCardLinksData = JSON.parse(savedCardLinksData);
         }
       }
-      console.log('theCardLinksData: ', theCardLinksData);
+      //console.log('theCardLinksData: ', theCardLinksData);
       return theCardLinksData;
     } catch (error) {
       console.error('Session storage error:', error);
